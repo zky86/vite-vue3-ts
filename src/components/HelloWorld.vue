290dd56ed0e502f2693fee4dbebc6e6ba535aab2
zky <kangyuanzheng@infinite.net.cn>
2021-12-27 16:36:06 +0800

    初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+
+
+<template>
+  <div class="main">
+    <h1>{{ msg }}</h1>
+    <button type="button" @click="count++">count is: {{ count }}</button>
+    <div>{{ book.title }}</div>
+  </div>
+</template>
+
+
+<script>
+  import { ref, reactive, onMounted } from 'vue'
+  export default {
+    props: ['msg'],
+    setup(props) {
+      // console.log(props)
+      const book = reactive({ title: 'Vue 3 Guide' })
+      const count = ref(0)
+      // console.log(count)
+      // 暴露给 template
+
+      onMounted(() => {
+         console.log("onMounted")
+      })
+
+      return {
+        book,
+        count
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.main{
+  h1{
+    color: #000;
+  }
+  a {
+  color: #42b983;
+}
+}
+
+</style>
