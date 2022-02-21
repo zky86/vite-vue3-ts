9310779900217ec5fba5fe74daff21d5c142aaeb
zhengkangyuan <zhengkangyuan@163.com>
2022-02-21 16:00:28 +0800

    全局引入饿了么UI icon，以及表单写法

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,23 @@ import './styles/reset.scss'
 import './styles/element-theme.sass'
 import locale from 'element-plus/lib/locale/lang/zh-cn' // 中文
 import './styles/color.scss'
+import * as icons from '@element-plus/icons-vue'
 import router from './router'
 // import 'element-plus/dist/index.css'
 import App from './App.vue'
 // import store from './store'
 import { store, key } from './store'
 
-createApp(App)
-  .use(router)
-  .use(ElementPlus, { locale })
-  .use(store, key)
-  .mount('#app')
+const app = createApp(App)
+// eslint-disable-next-line no-shadow
+Object.keys(icons).forEach((key) => {
+  app.component(key, icons[key])
+})
+
+app.use(router).use(ElementPlus, { locale }).use(store, key).mount('#app')
+
+// createApp(App)
+//   .use(router)
+//   .use(ElementPlus, { locale })
+//   .use(store, key)
+//   .mount('#app')
--- src/views/product.tsx
+++ src/views/product.tsx
@@ -1,21 +1,89 @@
-import { defineComponent } from 'vue'
-import { ElEmpty, ElButton } from 'element-plus'
+import { defineComponent, reactive, ref } from 'vue'
+import { ElEmpty, ElButton, ElForm, ElFormItem } from 'element-plus'
 import { useRouter } from 'vue-router'
-import  useCounter  from './counter'
+import useCounter from './counter'
+import elm from './elm'
+import {
+  ElInput
+} from 'element-plus'
 export default defineComponent({
   setup() {
     const router = useRouter()
     const { count, increment } = useCounter({})
+    const data = reactive({
+      user: {
+        name: '',
+        password: ''
+      }
+    })
+    const loginForm = ref<typeof ElForm | null>(null)
+
+    const login = () => {
+      if (!loginForm.value) return
+      loginForm.value.validate((valid: boolean) => {
+        if (valid) {
+          if (data.user.name === 'admin' && data.user.password === '123456') {
+            console.log('登录成功')
+          } else {
+            console.log('登录失败')
+          }
+        } else {
+          return false
+        }
+      })
+    }
+
+    const keyUp = ({ code }: KeyboardEvent) => {
+      if (code === 'Enter') {
+        login()
+      }
+    }
+
     return () => (
       <>
         <div>组件测试1：</div>
         <div>
-          You clicked: { count.value } times
+          You clicked: {count.value} times
           <button onClick={increment}>
             Click me
           </button>
         </div>
+        <br></br>
         <div>组件测试2</div>
+
+        <el-form ref={loginForm} model={data.user} label-width="120px" style="width:400px">
+          {/* <el-form-item label="用户名：">
+            <el-input  v-model={data.user.name}  placeholder="请输入" prefix-icon="user"></el-input>
+          </el-form-item> */}
+          <ElFormItem
+            label="账号："
+            prop="name"
+            rules={[{ required: true, message: '请输入用户名', trigger: 'blur' }]}
+          >
+            <ElInput
+              placeholder="请输入"
+              v-model={data.user.name}
+              v-slots={{
+                prefix: <i class="el-input__icon el-icon-user"></i>
+              }}
+              {...{
+                onKeyup: keyUp
+              }}
+            ></ElInput>
+          </ElFormItem>
+
+          <el-form-item label="密码：" prop="password" rules={[{ required: true, message: '请输入密码', trigger: 'blur' }]}>
+            <el-input v-model={data.user.password} placeholder="请输入" prefix-icon="unlock" ></el-input>
+          </el-form-item>
+        </el-form>
+
+        <ElButton type="primary" size="mini"
+          {...{
+            onClick: login
+          }}>提交
+        </ElButton>
+
+        <br></br>
         <ElEmpty description="404 NOT FOUND" class="404-page">
           <ElButton
             type="primary"
